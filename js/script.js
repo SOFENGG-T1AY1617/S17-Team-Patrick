6ec70315cb218a0c2d050f435d2ecbb1bf7ee592
Patrick Gan <patrickcuagan@gmail.com>
2016-10-17 23:19:49 +0800

    Fixed document_list divider, added jquery to autohide mailing if pickup is chosen, linked files together

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function resizeWindow() {
   myWindow.resizeTo(800, 600);
   myWindow.focus();
 }
+
+$("#pickup").click(function() {
+  $(".delivery-info").css("display", "none");
+})
+
+$("#delivery").click(function() {
+  $(".delivery-info").css("display", "");
+})
