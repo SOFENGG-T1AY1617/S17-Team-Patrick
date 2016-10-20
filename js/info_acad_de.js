5415f042f9584546640d6b551946c3a0f5e3a60c
Jason Abadejos <jason_abadejos@dlsu.edu.ph>
2016-10-20 21:00:38 +0800

    Added Checkbox for isGraduate
    Added automatic checked for Masters and Doctorate

--- js/info_acad_de.js
+++ js/info_acad_de.js
@@ -1,3 +1,11 @@
 for(var i=1960; i<=2016; i++) {
     $('#year_admitted').append($('<option>', {value:i, text:i}));
 }
+
+$("#masters").click(function() {
+  $("#cbGraduate").prop("checked", true);
+})
+
+$("#doctorate").click(function() {
+  $(".#cbGraduate").prop("checked", true);
+})
