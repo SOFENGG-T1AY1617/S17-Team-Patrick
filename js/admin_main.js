fb46048d0b55124e95783da8c3c284fe0142aef4
TylerVenzon <justin_venzon@dlsu.edu.ph>
2016-10-21 00:57:56 +0800

    Mark as Processing for Pending and "of -> For"

--- js/admin_main.js
+++ js/admin_main.js
@@ -7,6 +7,7 @@ $(".details").on('click', function() {
   $(".details-popup").css("display", "block");
   $(".content-scrolling").css("overflow-y", "hidden");
 })
+
 $("#cancel_popup").on('click', function() {
   $(".popup").css("display", "none");
 })
@@ -23,4 +24,17 @@ $("#back_popup").on('click', function() {
 $("#markasdone_popup").on('click', function() {
   $(".details-popup").css("display", "none");
   $(".content-scrolling").css("overflow-y", "scroll");
+})
+
+$(".info").on('click', function() {
+  $(".pending-popup").css("display", "block");
+  $(".content-scrolling").css("overflow-y", "hidden");
+})
+
+$("#mark_processing_popup").on('click', function() {
+  $(".pending-popup").css("display", "none");
+})
+
+$("#cancelprocess_popup").on('click', function() {
+  $(".pending-popup").css("display", "none");
 })
