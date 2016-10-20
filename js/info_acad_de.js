d34fc57400423a9ca41a07afdfc9151bcff829bc
Jason Abadejos <jason_abadejos@dlsu.edu.ph>
2016-10-20 21:41:41 +0800

    Added Uncheck Function
    If bachelor gets checked, isGraduate gets unchecked.

--- js/info_acad_de.js
+++ js/info_acad_de.js
@@ -2,6 +2,10 @@ for(var i=1960; i<=2016; i++) {
     $('#year_admitted').append($('<option>', {value:i, text:i}));
 }
 
+$("#bachelor").click(function() {
+  $("#cbGraduate").prop("checked", false);
+})
+
 $("#masters").click(function() {
   $("#cbGraduate").prop("checked", true);
 })
