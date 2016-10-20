f6c059698df76ca3927f53ad65ad245c6c47c166
Patrick Gan <patrickcuagan@gmail.com>
2016-10-20 22:06:34 +0800

    Added pending reason popup, fixed css

---
+++ js/admin_main.js
@@ -0,0 +1,11 @@
+$(".pending").on('click', function() {
+  $(".pending-popup").css("display", "block");
+})
+
+$("#cancel_popup").on('click', function() {
+  $(".pending-popup").css("display", "none");
+})
+
+$("#save_popup").on('click', function() {
+  $(".pending-popup").css("display", "none");
+})
