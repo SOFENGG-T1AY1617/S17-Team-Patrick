4c21325190097718482d946be93e4a42c287c5e0
Patrick Gan <patrickcuagan@gmail.com>
2016-10-20 22:33:48 +0800

    Changed class name

--- js/admin_main.js
+++ js/admin_main.js
@@ -1,11 +1,11 @@
 $(".pending").on('click', function() {
-  $(".pending-popup").css("display", "block");
+  $(".popup").css("display", "block");
 })
 
 $("#cancel_popup").on('click', function() {
-  $(".pending-popup").css("display", "none");
+  $(".popup").css("display", "none");
 })
 
 $("#save_popup").on('click', function() {
-  $(".pending-popup").css("display", "none");
+  $(".popup").css("display", "none");
 })
