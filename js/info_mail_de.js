fa65b50f5a78ec690530634bff128e68e6e50c78
Patrick Gan <patrickcuagan@gmail.com>
2016-10-20 23:38:45 +0800

    Removed mode of retrieval

--- js/info_mail_de.js
+++
@@ -1,9 +0,0 @@
-$("#pickup").click(function() {
-  $(".delivery-info").css("display", "none");
-  $(".pickup-info").css("display", "");
-})
-
-$("#delivery").click(function() {
-  $(".pickup-info").css("display", "none");
-  $(".delivery-info").css("display", "");
-})
