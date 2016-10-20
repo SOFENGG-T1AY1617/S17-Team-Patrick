db371990e8f3fff32d8c824a3a2be71acd7152f6
Jason Abadejos <jason_abadejos@dlsu.edu.ph>
2016-10-20 22:58:43 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	info_mail_de.html

--- js/admin_main.js
+++ js/admin_main.js
@@ -1,11 +1,26 @@
 $(".pending").on('click', function() {
-  $(".pending-popup").css("display", "block");
+  $(".popup").css("display", "block");
+})
+
+$(".details").on('click', function() {
+  $(".details-popup").css("display", "block");
+  $(".content-scrolling").css("overflow-y", "hidden");
 })
 
 $("#cancel_popup").on('click', function() {
-  $(".pending-popup").css("display", "none");
+  $(".popup").css("display", "none");
 })
 
 $("#save_popup").on('click', function() {
-  $(".pending-popup").css("display", "none");
+  $(".popup").css("display", "none");
+})
+
+$("#back_popup").on('click', function() {
+  $(".details-popup").css("display", "none");
+  $(".content-scrolling").css("overflow-y", "scroll");
+})
+
+$("#markasdone_popup").on('click', function() {
+  $(".details-popup").css("display", "none");
+  $(".content-scrolling").css("overflow-y", "scroll");
 })
