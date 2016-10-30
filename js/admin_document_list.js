c14c14ae8c9a3d6f27eaad6ab330168d1c4a6344
Patrick Gan <patrickcuagan@gmail.com>
2016-10-30 21:31:14 +0800

    Added missing folders

---
+++ js/admin_document_list.js
@@ -0,0 +1,27 @@
+$(".edit_button").on('click', function() {
+  $(".edit").css("display", "block");
+})
+
+$("#yes_edit, #no_edit").on('click', function() {
+  $(".edit").css("display", "none");
+});
+
+$("#add_button").on('click', function() {
+  $(".add").css("display", "block");
+});
+
+$("#yes_add, #no_add").on('click', function() {
+  $(".add").css("display", "none");
+});
+
+$("#delete_button").on('click', function() {
+  $(".delete").css("display", "block");
+});
+
+$("#yes_delete, #no_delete").on('click', function() {
+  $(".delete").css("display", "none");
+});
+
+$(".available_express").on('click', function() {
+  $(".express_charge").toggleClass("hidden");
+})
