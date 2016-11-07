612a44bac0c5f0231b69dc92f7d0d9ddb5b96836
Jan Joseph <Jan Joseph@JANJOSEPH>
2016-11-08 00:37:16 +0800

    Implemented Interface to DocumentList

--- SOFENGG Order Request Document/View/Order/DocumentList.aspx.cs
+++ SOFENGG Order Request Document/View/Order/DocumentList.aspx.cs
@@ -2,10 +2,63 @@
 
 namespace SOFENGG_Order_Request_Document.View.Order
 {
-    public partial class DocumentList : System.Web.UI.Page
+    public partial class DocumentList : System.Web.UI.Page, IDocumentList
     {
+        public string Category
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Document
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string ExpressPrice
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string RegularPrice
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
 
         }
     }
