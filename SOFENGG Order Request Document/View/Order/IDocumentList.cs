0f4cc427e791298a12cdf56e3aebbf95b9922add
Jan Joseph <Jan Joseph@JANJOSEPH>
2016-11-08 00:26:30 +0800

    Added Interface for DocumentList (IDocumentList.cs)

---
+++ SOFENGG Order Request Document/View/Order/IDocumentList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOFENGG_Order_Request_Document.View.Order
+{
+    interface IDocumentList
+    {
+        string Document { get; set; }
+        string Category { get; set; }
+        string RegularPrice { get; set; }
+        string ExpressPrice { get; set; }
+    }
+}
