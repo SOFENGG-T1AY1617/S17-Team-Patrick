b0d4b3e7be62873d0684940e676f593433804ff0
Jan Joseph <Jan Joseph@JANJOSEPH>
2016-11-08 00:27:48 +0800

    Added Model (OrderItemWithTerm.cs)

---
+++ SOFENGG Order Request Document/Model/OrderItemWithTerm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOFENGG_Order_Request_Document.Model
+{
+	public class OrderItemWithTerm
+	{
+
+	}
+}
