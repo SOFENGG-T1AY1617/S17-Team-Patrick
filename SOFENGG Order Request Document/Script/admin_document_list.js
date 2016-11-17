bf095e4a159319b9f197c2c4a66e08f00365417b
Patrick Gan <patrickcuagan@gmail.com>
2016-11-17 20:44:39 +0800

    Fixed bug wherein modal forms are being retained once you close and open it.

--- SOFENGG Order Request Document/Script/admin_document_list.js
+++ SOFENGG Order Request Document/Script/admin_document_list.js
@@ -1,35 +1,27 @@
-//function clear_form_elements(class_name) {
-//    $("." + class_name).find(':input').each(function () {
-//        switch (this.type) {
-//            case 'password':
-//            case 'text':
-//            case 'textarea':
-//            case 'file':
-//            case 'select-one':
-//            case 'select-multiple':
-//            case 'date':
-//            case 'number':
-//            case 'tel':
-//            case 'email':
-//                jQuery(this).val('');
-//                break;
-//            case 'checkbox':
-//            case 'radio':
-//                this.checked = false;
-//                break;
-//        }
-//    });
-//}
+function clear_form_elements(class_name) {
+    $("." + class_name).find(':input').each(function () {
+        switch (this.type) {
+            case 'password':
+            case 'text':
+            case 'textarea':
+            case 'file':
+            case 'select-one':
+            case 'select-multiple':
+            case 'date':
+            case 'number':
+            case 'tel':
+            case 'email':
+                jQuery(this).val('');
+                break;
+            case 'checkbox':
+            case 'radio':
+                this.checked = false;
+                break;
+        }
+    });
+}
 
-//$("#add_button").on('click', function () {
-//    $(".popup-background").height($(document).height);
-//    clear_form_elements("form-horizontal");
-//    $(".popup-background").toggle();
-
-//    return false;
-//});
-
-//$("#btnAdd, #btnAddCancel").on('click', function () {
-//    $(".popup-background").toggle();
-//    return false;
-//});
+$("#add_document_modal").on('click', function () {
+    clear_form_elements("col-sm-5");
+    clear_form_elements("checkbox");
+});
