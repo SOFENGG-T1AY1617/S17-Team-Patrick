8a99464c445b43654e072324eb67535a73601fd2
CudoXY <gelo103097@yahoo.com>
2016-11-18 09:22:22 +0800

    Added missing JS files

---
+++ SOFENGG Order Request Document/Script/admin_document_list.js
@@ -0,0 +1,12 @@
+$("#add_button").on('click', function () {
+    $('.popup-background').height($(document).height());
+    $(".popup-background").css("display", "block");
+    event.preventDefault();
+    return false;
+});
+
+$("#btnAdd, #btnAddCancel").on('click', function () {
+    $(".popup-background").css("display", "none");
+    event.preventDefault();
+    return false;
+});
